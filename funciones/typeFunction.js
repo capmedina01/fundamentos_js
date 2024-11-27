b7df958e73155ec4c3ae348c32eadbb0314f300c
Carlos <carlos_aperdomo@soy.sena.edu.co>
2024-11-26 22:18:31 -0500

    funciones como argumento o parametro

--- funciones/typeFunction.js
+++ funciones/typeFunction.js
@@ -1,3 +1,9 @@
 // capacidade de las funciones
 
 //1) Pasar funciones como argumentos o parametro----tambien se le llama --callback--
+
+function a () {}
+function b (a) {}
+b(a);
+
+// retornar fuciones
