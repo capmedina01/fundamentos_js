1f2ae8bb26c335ce548710a201a35fb08e69ef17
perdomomedina.carlos@gmail.com <perdomomedina.carlos@gmail.com>
2024-11-28 16:27:36 -0500

    closure

---
+++ funciones/closures.js
@@ -0,0 +1,9 @@
+/**
+ * closure: funcion que tiene acceso a variables de un ambito externo,
+ *          incluso cuando esa funcion haya terminado de ejecutarse.
+ * 
+ * Ambito lexico: cada vez que se declara una funcion crea su propio ambito lexico,
+ *                y puede acceder a las variables dentro de ese ambito y a variables
+ *                de ambitos superiores.
+*/
+
